017b29b99998a3db39e26241f0acf93b14431ff9
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-06-21 16:41:48 +0100

    Added Fancybox to banner img

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!--Fancy Box -->
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
+
     <title>Home | SolarFrame</title>
     <meta name="description" content="Take a look at our conservatories, including our amazing lightweight solid roof replacements, and orangeries. You can’t beat the quality of our windows...">
 
@@ -21,7 +25,7 @@
         <div class="grid grid-cols-12">
             <div class="col-span-2">
                 <a href="#" title="Home" aria-label="Home">
-                    <img src="" alt="SolarFrame Logo" class="w-full h-auto">
+                    <img src="/images/logo.svg" alt="SolarFrame Logo" class="w-full h-auto">
                 </a>
             </div>
             <div class="col-span-2">
@@ -29,7 +33,7 @@
                 <a href="mailto:info@solarframe-direct.co.uk" title="Email" aria-label="Email" class="block"><i class="fa fa-phone" aria-hidden="true"></i>info@solarframe-direct.co.uk</a>
             </div>
             <div class="col-span-1 col-start-8">
-                <a href="https://trustedtraders.which.co.uk/businesses/solarframe-direct-limited/" title="which review" aria-label="which review" target="_blank"><img src="#" alt="which review"></a>
+                <a href="https://trustedtraders.which.co.uk/businesses/solarframe-direct-limited/" title="which review" aria-label="which review" target="_blank"><img src="/images/which-logo.jpeg" alt="which review"></a>
             </div>
             <div class="col-span-1">
                 <a href="#" title="request a quote" aria-label="request a quote">Request a quote</a>
@@ -45,18 +49,18 @@
             </div>
         </div>
         <div class="container">
-            <div class="grid grid-cols-10">
-                <a class="inline" href="#" title="Roof Replacements" aria-label="Roof Replacements">Roof Replacements</a>
-                <a class="inline" href="#" title="Conservatories" aria-label="Conservatories">Conservatories</a>
-                <a class="inline" href="#" title="Orangeries" aria-label="Orangeries">Orangeries</a>
-                <a class="inline" href="#" title="Windows" aria-label="Windows">Windows</a>
-                <a class="inline" href="#" title="Doors" aria-label="Doors">Doors</a>
-                <a class="inline" href="#" title="Garden Studios" aria-label="Garden Studios">Garden Studios</a>
-                <a class="inline" href="#" title="Showrooms" aria-label="Showrooms">Showrooms</a>
-                <a class="inline" href="#" title="Finance Options" aria-label="Finance Options">Finance Options</a>
-                <a class="inline" href="#" title="Inspiration" aria-label="Inspiration">Inspiration</a>
-                <a class="inline" href="#" title="About" aria-label="About">About</a>
-            </div>
+            <ul class="grid grid-cols-10">
+                <li><a class="inline" href="#" title="Roof Replacements" aria-label="Roof Replacements">Roof Replacements</a></li>
+                <li><a class="inline" href="#" title="Conservatories" aria-label="Conservatories">Conservatories</a></li>
+                <li><a class="inline" href="#" title="Orangeries" aria-label="Orangeries">Orangeries</a></li>
+                <li><a class="inline" href="#" title="Windows" aria-label="Windows">Windows</a></li>
+                <li><a class="inline" href="#" title="Doors" aria-label="Doors">Doors</a></li>
+                <li><a class="inline" href="#" title="Garden Studios" aria-label="Garden Studios">Garden Studios</a></li>
+                <li><a class="inline" href="#" title="Showrooms" aria-label="Showrooms">Showrooms</a></li>
+                <li><a class="inline" href="#" title="Finance Options" aria-label="Finance Options">Finance Options</a></li>
+                <li><a class="inline" href="#" title="Inspiration" aria-label="Inspiration">Inspiration</a></li>
+                <li><a class="inline" href="#" title="About" aria-label="About">About</a></li>
+            </ul>
         </div>
         <div class="grid grid-cols-1">
             <div class="col-span-1">
@@ -64,5 +68,20 @@
             </div>
         </div>
     </header>
+    <section id="banner">
+        <div class="grid grid-cols-1">
+            <div class="col-span-1">
+                <a href="/images/banner-img.jpeg" data-fancybox="gallery" data-caption="Caption #1">
+                    <img src="/images/banner-img.jpeg" />
+                </a>
+            </div>
+        </div>
+    </section>
+    <main>
+
+    </main>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="./scripts/scripts.js"></script>
 </body>
 </html>
