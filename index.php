3cb9f8a69edaed1d0e3ca6d0039f18e4af6087f3
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-06-21 15:56:28 +0100

    Header html done

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/dist/tailwind.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Home | SolarFrame</title>
+    <meta name="description" content="Take a look at our conservatories, including our amazing lightweight solid roof replacements, and orangeries. You can’t beat the quality of our windows...">
+
+</head>
+<body class="font-Manrope">
+    <a class="skip-to-the-main-content hidden" href="#content" title="Skip to the main content" aria-label="Skip to the main content" tabindex="0">Skip to main content</a>
+    <header id="site header">
+        <div class="grid grid-cols-12">
+            <div class="col-span-2">
+                <a href="#" title="Home" aria-label="Home">
+                    <img src="" alt="SolarFrame Logo" class="w-full h-auto">
+                </a>
+            </div>
+            <div class="col-span-2">
+                <a href="tel:08006250000" title="Phone" aria-label="Phone" class="block"><i class="fa fa-phone" aria-hidden="true"></i>0800 6250000</a>
+                <a href="mailto:info@solarframe-direct.co.uk" title="Email" aria-label="Email" class="block"><i class="fa fa-phone" aria-hidden="true"></i>info@solarframe-direct.co.uk</a>
+            </div>
+            <div class="col-span-1 col-start-8">
+                <a href="https://trustedtraders.which.co.uk/businesses/solarframe-direct-limited/" title="which review" aria-label="which review" target="_blank"><img src="#" alt="which review"></a>
+            </div>
+            <div class="col-span-1">
+                <a href="#" title="request a quote" aria-label="request a quote">Request a quote</a>
+            </div>
+            <div class="col-span-1">
+                <a href="#" title="Book a survey" aria-label="Book a survey">Book a survey</a>
+            </div>
+            <div class="col-span-1">
+                <a href="#" title="Visit showroom" aria-label="Visit showroom">Visit showroom</a>
+            </div>
+            <div class="col-span-1">
+                <a href="#" title="Search" aria-label="Search"><i class="fa fa-search" aria-hidden="true"></i></a>
+            </div>
+        </div>
+        <div class="container">
+            <div class="grid grid-cols-10">
+                <a class="inline" href="#" title="Roof Replacements" aria-label="Roof Replacements">Roof Replacements</a>
+                <a class="inline" href="#" title="Conservatories" aria-label="Conservatories">Conservatories</a>
+                <a class="inline" href="#" title="Orangeries" aria-label="Orangeries">Orangeries</a>
+                <a class="inline" href="#" title="Windows" aria-label="Windows">Windows</a>
+                <a class="inline" href="#" title="Doors" aria-label="Doors">Doors</a>
+                <a class="inline" href="#" title="Garden Studios" aria-label="Garden Studios">Garden Studios</a>
+                <a class="inline" href="#" title="Showrooms" aria-label="Showrooms">Showrooms</a>
+                <a class="inline" href="#" title="Finance Options" aria-label="Finance Options">Finance Options</a>
+                <a class="inline" href="#" title="Inspiration" aria-label="Inspiration">Inspiration</a>
+                <a class="inline" href="#" title="About" aria-label="About">About</a>
+            </div>
+        </div>
+        <div class="grid grid-cols-1">
+            <div class="col-span-1">
+                <p>Up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer - up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer -</p>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./*.php",
+    "./components/**/*.php"
+  ],
+  safelist: [
+    {
+      pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+      variants: ['sm', 'md', 'lg', 'xl', '2xl']
+
+    },
+    {
+      pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+      variants: ['sm', 'md', 'lg', 'xl', '2xl']
+    },
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1400px',
+      '2xlx': '1600px'
+    },
+    extend: {
+      colors: {
+        'brand-orange': '#fd8204',
+        'brand-lightgrey': '#9fa2a5',
+        'brand-grey': '#464f53',
+        'brand-darkgrey': '#343a40',
+        'brand-black': '#222',
+        'brand-white': '#fff',
+      },
+      fontSize: {
+        h1: ['45px', '53px'],
+        h2: ['35px', '40px'],
+        p: ['16px', '25px'],
+      },
+      fontFamily: {
+        'Manrope': "'Manrope', 'sans-serif'"
+      }
+    },
   },
   plugins: [],
 }
+
 
