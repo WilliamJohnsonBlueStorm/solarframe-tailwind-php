5a38f28db0a86436595015cd347e557c592479d3
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-06-23 14:38:51 +0100

    finished header

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!--Fancy Box -->
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
 
     <title>Home | SolarFrame</title>
@@ -21,63 +20,81 @@
 </head>
 <body class="font-Manrope text-p">
     <a class="skip-to-the-main-content hidden" href="#content" title="Skip to the main content" aria-label="Skip to the main content" tabindex="0">Skip to main content</a>
-    <header id="site header">
+    <header id="site-header">
         <div class="container">
             <div class="grid grid-cols-12">
-                <div class="col-span-2">
-                    <a href="#" title="Home" aria-label="Home">
-                        <img src="/images/logo.svg" alt="SolarFrame Logo" class="w-full h-auto">
-                    </a>
+                <div class="col-span-4">
+                    <div class="grid grid-cols-2 items-center gap-8">
+                        <div class="col-span-1">
+                            <a href="#" title="Home" aria-label="Home">
+                                <img src="/images/logo.svg" alt="SolarFrame Logo" class="w-full h-auto">
+                            </a>
+                        </div>
+                        <div class="col-span-1">
+                            <a href="tel:08006250000" title="Phone" aria-label="Phone" class="block py-2"><i class="fa fa-phone text-brand-orange mr-2" aria-hidden="true"></i>0800 6250000</a>
+                            <a href="mailto:info@solarframe-direct.co.uk" title="Email" aria-label="Email" class="block py-2"><i class="fa fa-envelope text-brand-orange mr-2" aria-hidden="true"></i>info@solarframe-direct.co.uk</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-span-2">
-                    <a href="tel:08006250000" title="Phone" aria-label="Phone" class="block"><i class="fa fa-phone" aria-hidden="true"></i>0800 6250000</a>
-                    <a href="mailto:info@solarframe-direct.co.uk" title="Email" aria-label="Email" class="block"><i class="fa fa-phone" aria-hidden="true"></i>info@solarframe-direct.co.uk</a>
-                </div>
-                <div class="col-span-1 col-start-8">
-                    <a href="https://trustedtraders.which.co.uk/businesses/solarframe-direct-limited/" title="which review" aria-label="which review" target="_blank"><img src="/images/which-logo.jpeg" alt="which review"></a>
-                </div>
-                <div class="col-span-1">
-                    <a href="#" title="request a quote" aria-label="request a quote">Request a quote</a>
-                </div>
-                <div class="col-span-1">
-                    <a href="#" title="Book a survey" aria-label="Book a survey">Book a survey</a>
-                </div>
-                <div class="col-span-1">
-                    <a href="#" title="Visit showroom" aria-label="Visit showroom">Visit showroom</a>
-                </div>
-                <div class="col-span-1">
-                    <a href="#" title="Search" aria-label="Search"><i class="fa fa-search" aria-hidden="true"></i></a>
+                <div class="col-span-8">
+                    <ul class="flex justify-end h-full">
+                        <li class="flex items-center align-middle">
+                            <a href="https://trustedtraders.which.co.uk/businesses/solarframe-direct-limited/" title="which review" aria-label="which review" target="_blank" class="p-5 flex items-center">
+                                <img src="/images/which-logo.jpeg" alt="which review" class="max-h-[50px] inline" />
+                            </a>
+                        </li>
+                        <li class="flex items-stretch">
+                            <a class="uppercase bg-brand-orange text-white p-5 flex items-center" href="#" title="request a quote" aria-label="request a quote">
+                                Request a quote
+                            </a>
+                        </li>
+                        <li class="flex items-stretch">
+                            <a class="uppercase bg-brand-lightgrey text-white p-5 flex items-center" href="#" title="Book a survey" aria-label="Book a survey">
+                                Book a survey
+                            </a>
+                        </li>
+                        <li class="flex items-stretch">
+                            <a class="uppercase bg-brand-black text-white p-5 flex items-center" href="#" title="Visit showroom" aria-label="Visit showroom">
+                                Visit showroom
+                            </a>
+                        </li>
+                        <li class="flex items-stretch">
+                            <a class="bg-brand-grey p-5 text-white flex items-center" href="#" title="Search" aria-label="Search">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <div>
-            <nav class="container" role="navigation">
-                <ul class="grid grid-cols-12">
-                    <li class="inline col-start-2"><a href="#" title="Roof Replacements" aria-label="Roof Replacements">Roof Replacements</a></li>
-                    <li class="inline"><a href="#" title="Conservatories" aria-label="Conservatories">Conservatories</a></li>
-                    <li class="inline"><a href="#" title="Orangeries" aria-label="Orangeries">Orangeries</a></li>
-                    <li class="inline"><a href="#" title="Windows" aria-label="Windows">Windows</a></li>
-                    <li class="inline"><a href="#" title="Doors" aria-label="Doors">Doors</a></li>
-                    <li class="inline"><a href="#" title="Garden Studios" aria-label="Garden Studios">Garden Studios</a></li>
-                    <li class="inline"><a href="#" title="Showrooms" aria-label="Showrooms">Showrooms</a></li>
-                    <li class="inline"><a href="#" title="Finance Options" aria-label="Finance Options">Finance Options</a></li>
-                    <li class="inline"><a href="#" title="Inspiration" aria-label="Inspiration">Inspiration</a></li>
-                    <li class="inline"><a href="#" title="About" aria-label="About">About</a></li>
-                </ul>
-            </nav>
+        <div class="bg-brand-darkgrey text-brand-white">
+            <div class="container" role="navigation">
+                <div class="grid grid-cols-12">
+                    <nav class="col-span-12">
+                        <ul class="flex flex-row justify-center gap-4">
+                            <li class="inline"><a href="#" title="Roof Replacements" aria-label="Roof Replacements" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Roof Replacements</a></li>
+                            <li class="inline"><a href="#" title="Conservatories" aria-label="Conservatories" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Conservatories</a></li>
+                            <li class="inline"><a href="#" title="Orangeries" aria-label="Orangeries" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Orangeries</a></li>
+                            <li class="inline"><a href="#" title="Windows" aria-label="Windows" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Windows</a></li>
+                            <li class="inline"><a href="#" title="Doors" aria-label="Doors" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Doors</a></li>
+                            <li class="inline"><a href="#" title="Garden Studios" aria-label="Garden Studios" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Garden Studios</a></li>
+                            <li class="inline"><a href="#" title="Showrooms" aria-label="Showrooms" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Showrooms</a></li>
+                            <li class="inline"><a href="#" title="Finance Options" aria-label="Finance Options" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Finance Options</a></li>
+                            <li class="inline"><a href="#" title="Inspiration" aria-label="Inspiration" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">Inspiration</a></li>
+                            <li class="inline"><a href="#" title="About" aria-label="About" class="p-4 inline-block hover:text-brand-orange transition-all duration-300">About</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
         <div class="grid grid-cols-1">
-            <div class="col-span-1">
-                <p>Up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer - up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer -</p>
-            </div>
+            <p>Up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer - up to 60% off the entire solarframe range - interest free credit available - transform your home in time for summer -</p>
         </div>
     </header>
     <section id="banner" role="banner">
         <div class="grid grid-cols-1 swiper-slide">
             <div class="col-span-1">
-                <div>
-                    <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
-                </div>
+                <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
             </div>
             <div class="container">
                 <div class="grid grid-cols-12">
@@ -91,9 +108,7 @@
         </div>
         <div class="grid grid-cols-1 swiper-slide hidden">
             <div class="col-span-1">
-                <div>
-                    <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
-                </div>
+                <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
             </div>
             <div class="container">
                 <div class="grid grid-cols-12">
@@ -107,9 +122,7 @@
         </div>
         <div class="grid grid-cols-1 swiper-slide hidden">
             <div class="col-span-1">
-                <div>
-                    <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
-                </div>
+                <img src="/images/main-banner-img.jpeg" alt="banner image" class="w-full h-auto"/>
             </div>
             <div class="container">
                 <div class="grid grid-cols-12">
@@ -442,6 +455,7 @@
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
     <script src="./scripts/scripts.js"></script>
 </body>
 </html>
