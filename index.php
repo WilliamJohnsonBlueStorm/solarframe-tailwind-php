4048acef1e18bb694338edccce51a2f5da67fa48
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-06-28 16:34:11 +0100

    finished conservatories, orangeries and extensions rows on image grid

--- index.php
+++ index.php
@@ -261,9 +261,9 @@
                     </div>
                 </div>
                 <div class="grid grid-cols-3">
-                    <div class="col-span-1 border border-brand-white overflow-hidden">
+                    <div class="col-span-3 lg:col-span-1 border border-brand-white overflow-hidden">
                         <a href="#" title="Conservatories" aria-label="Conservatories" class="relative group">
-                            <img src="images/conservatories.webp" alt="Conservatories" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300">
+                            <img src="images/conservatories.webp" alt="Conservatories" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300 max-h-[300px] w-full object-cover">
                             <div class="absolute bottom-0 p-4 text-brand-white">
                                 <h3 class="text-h3 mb-4">Conservatories</h3>
                                 <p class="hidden mb-4 group-hover:block transition-all duration-300">Conservatories provide a comfortable and stylish addition to any home.</p>
@@ -274,9 +274,9 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-span-1 border border-brand-white overflow-hidden">
+                    <div class="col-span-3 lg:col-span-1 border border-brand-white overflow-hidden">
                         <a href="#" title="Orangeries" aria-label="Orangeries" class="relative group">
-                            <img src="images/orangeries.jpeg" alt="Orangeries" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300">
+                            <img src="images/orangeries.jpeg" alt="Orangeries" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300 max-h-[300px] w-full object-cover">
                             <div class="absolute bottom-0 p-4 text-brand-white">
                                 <h3 class="text-h3 mb-4">Orangeries</h3>
                                 <p class="hidden mb-4 group-hover:block transition-all duration-300">State-of-the-Art Orangeries - Make an instant impression</p>
@@ -287,12 +287,12 @@
                             </div>
                         </a>
                     </div>
-                    <div class="col-span-1 border border-brand-white overflow-hidden">
+                    <div class="col-span-3 lg:col-span-1 border border-brand-white overflow-hidden">
                         <a href="#" title="Extensions" aria-label="Extensions" class="relative group">
-                            <img src="images/extensions.jpeg" alt="Extensions" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300">
+                            <img src="images/extensions.jpeg" alt="Extensions" class="brightness-90 group-hover:brightness-75 group-hover:scale-110 transition-all duration-300 max-h-[300px] w-full object-cover">
                             <div class="absolute bottom-0 p-4 text-brand-white">
-                                <h3 class="text-h3 mb-4">Conservatories</h3>
-                                <p class="hidden mb-4 group-hover:block transition-all duration-300">Conservatories provide a comfortable and stylish addition to any home.</p>
+                                <h3 class="text-h3 mb-4">Single Story Extensions</h3>
+                                <p class="hidden mb-4 group-hover:block transition-all duration-300">Our extensions are built to last and completely bespoke to your requirements</p>
                                 <div class="flex">
                                     <i class="fa fa-arrow-right h-[35px] w-[35px] border border-brand-orange rounded-full !flex items-center justify-center mr-2 group-hover:bg-brand-orange transition-all duration-300" aria-hidden="true"></i>
                                     <p class="flex items-center">More information</p>
